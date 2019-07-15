fc9fbd7773cd9a8636a4451054ce0e0aef8eb024
maruthu007 <52648113+maruthu007@users.noreply.github.com>
2019-07-15 11:52:11 +0530

    Update vowel1.py

--- vowel1.py
+++ vowel1.py
@@ -2,7 +2,7 @@ ma=input()
 if (ma>='a' and ma<='z') or (ma>='A' and ma<='Z'):
     if ma in ('A','E','I','O','U','a','e','i','o','u'):
         print("Vowel")
-    elif ma not in('b','c','I','O','U','a','e','i','o','u'):
+    elif ma not in('A','E','I','O','U','a','e','i','o','u'):
         print("Consonant")
 else:
     print("invalid")
